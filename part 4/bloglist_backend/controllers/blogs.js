27852fcc5baaa387136d127841c7beecf0a51fcc
Jayesh Jain <79307894+jayesh-jain252@users.noreply.github.com>
2021-12-07 09:07:31 +0530

    Update blogs.js

--- part 4/bloglist_backend/controllers/blogs.js
+++ part 4/bloglist_backend/controllers/blogs.js
@@ -24,7 +24,7 @@ blogsRouter.post('/',async (request, response, next) => {
     // const token = request.token
     const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-    if (!token || !decodedToken.id) {
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
     const user = await User.findById(decodedToken.id)
